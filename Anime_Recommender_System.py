ffb2fb3d27439225c72256931e42a6f191c03f0b
Alfian Syach <127624933+alfiansyach23@users.noreply.github.com>
2023-12-06 14:35:02 +0700

    Update Anime_Recommender_System.py

--- Anime_Recommender_System.py
+++ Anime_Recommender_System.py
@@ -17,8 +17,8 @@ anime_movies_file_path = os.path.join(project_dir, 'Anime_Recommender_System.csv
 anime_movies = pd.read_csv(anime_movies_file_path)
 
 df = anime_movies_scrapping[['Rank', 'Title', 'Rating', 'Image_URL', 'Episodes', 'Dates', 'Members']].merge(
-     anime_movies[['title', 'genres', 'studios', 'producers', 'synopsis']],
-     left_on='Title', right_on='title', how='left')
+    anime_movies[['title', 'genres', 'studios', 'producers', 'synopsis']],
+    left_on='Title', right_on='title', how='left')
 
 df.drop(columns='title', inplace=True)
 
@@ -26,7 +26,7 @@ df.rename(columns={'genres': 'Genres',
                    'studios': 'Studios',
                    'producers': 'Producers',
                    'synopsis': 'Synopsis'},
-                   inplace=True)
+          inplace=True)
 
 df.replace(r'^\s*$', np.nan, regex=True, inplace=True)
 df.dropna(inplace=True)
@@ -73,11 +73,12 @@ def determine_status(end_date):
 
 df['Status'] = df['EndDate'].apply(determine_status)
 
-col_order = ['Rank', 'Title', 'Rating', 'Type', 'Episodes', 'StartDate', 'EndDate', 'Status', 'Genres', 'Studios', 'Producers', 'Synopsis', 'Members']
+col_order = ['Rank', 'Title', 'Rating', 'Image_URL', 'Type', 'Episodes', 'StartDate', 'EndDate', 'Status', 'Genres', 'Studios', 'Producers', 'Synopsis', 'Members']
 df = df[col_order]
 
 df.replace(r'^\s*$', np.nan, regex=True, inplace=True)
 df.dropna(inplace=True)
+df.reset_index(inplace=True)
 
 # Content-based filtering
 df['Combined'] = df['Genres'] + ' ' + df['Studios'] + ' ' + df['Producers']
@@ -98,6 +99,13 @@ popularity_based_weight = 0.3
 df['Final_Score'] = content_based_weight * df['Content_Based_Score'] + popularity_based_weight * df['Popularity_Score']
 df_sorted_final = df.sort_values(by='Final_Score', ascending=False)
 
+def get_image_url(title, df):
+    matching_row = df[df['Title'].str.contains(title, case=False)]
+    if not matching_row.empty and not pd.isnull(matching_row.iloc[0]['Image_URL']):
+        return matching_row.iloc[0]['Image_URL']
+    else:
+        return None
+
 st.title('Anime Recommender System')
 
 anime_input = st.text_input('Enter Anime Title:')
@@ -113,22 +121,58 @@ def search(title, df_sorted_final, cosine_sim):
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     top_similar_indices = [i[0] for i in sim_scores[1:6]]
 
-    recommended_anime = df_sorted_final[['Title', 'Final_Score']].iloc[top_similar_indices]
+    recommended_anime = df_sorted_final[['Title', 'Genres', 'Synopsis', 'Final_Score', 'Image_URL']].iloc[top_similar_indices]
+    recommended_anime['Image_URL'] = recommended_anime['Title'].apply(lambda x: get_image_url(x, df_sorted_final))
     return recommended_anime
 
+def truncate_text(text, max_chars):
+    return (text[:max_chars] + '...') if len(text) > max_chars else text
+
 if st.button('Search'):
-    recommended_anime = search(anime_input, df_sorted_final, cosine_sim)
-    if recommended_anime is not None:
-        # Merge recommended_anime with the original DataFrame df based on the "Title" column
-        merged_df = pd.merge(recommended_anime, df[['Title', 'Rating']], on='Title', how='left')
+    matching_anime_titles = df_sorted_final[df_sorted_final['Title'].str.contains(anime_input, case=False)]['Title'].tolist()
 
-        # Sort merged_df based on the "Final_Score" column in descending order
-        merged_df = merged_df.sort_values(by='Final_Score', ascending=False)
-
-        st.markdown('<div style="background-color:#7B66FF;color:#ffffff;padding:10px;border-radius:5px;"><b>Anime Recommendation for You :</b></div>', unsafe_allow_html=True)
+    if matching_anime_titles:
+        st.markdown('<div style="background-color:#7B66FF;color:#ffffff;padding:10px;border-radius:5px;"><b>List of Suitable Anime Titles :</b></div>', unsafe_allow_html=True)
         st.write('')
 
-        for i, row in enumerate(merged_df.itertuples(), 1):
-            # Use "Rating" column for displaying star rating
-            rating = row.Rating if 'Rating' in row._fields else 'N/A'  # Handle the case where 'Rating' is not available
-            st.markdown(f'<div style="background-color:#F0F0F0;color:#000000;padding:10px;border-radius:5px;margin-bottom:8px;">{i}. {row.Title} (⭐ {rating:.2f})</div>', unsafe_allow_html=True)
+        for anime_title in matching_anime_titles[:5]:
+            col1, col2 = st.columns([1, 3])
+
+            anime_data = df_sorted_final[df_sorted_final['Title'] == anime_title].iloc[0]
+
+            image_url = get_image_url(anime_data['Title'], df_sorted_final)
+            if image_url:
+                col1.image(image_url, use_column_width=True, output_format='png')
+            else:
+                placeholder_image = "https://via.placeholder.com/150x228"
+                col1.image(placeholder_image, use_column_width=True, output_format='png')
+
+            genres_with_commas = ', '.join(anime_data['Genres'].split())
+            truncated_synopsis = truncate_text(anime_data['Synopsis'], max_chars=250)
+            
+            col2.write(f"**Title:** {anime_data['Title']}")
+            col2.markdown(f"**Rating:** ⭐ {anime_data['Final_Score']:.2f}\n\n**Genres:** {genres_with_commas}\n\n**Synopsis:** {truncated_synopsis}")
+
+    recommended_anime = search(anime_input, df_sorted_final, cosine_sim)
+
+    if recommended_anime is not None:
+        st.markdown('<div style="background-color:#7B66FF;color:#ffffff;padding:10px;border-radius:5px;"><b>Anime recommendations you might be looking for :</b></div>', unsafe_allow_html=True)
+        st.write('')
+
+        for i, row in enumerate(recommended_anime.itertuples(), 1):
+            rating = row.Final_Score
+            col1, col2 = st.columns([1, 3])
+
+            image_url = get_image_url(row.Title, df_sorted_final)
+            if image_url:
+                col1.image(image_url, use_column_width=True, output_format='png')
+            else:
+                placeholder_image = "https://via.placeholder.com/150x228"
+                col1.image(placeholder_image, use_column_width=True, output_format='png')
+
+            genres_with_commas = ', '.join(row.Genres.split())
+            truncated_synopsis = truncate_text(row.Synopsis, max_chars=250)
+            col2.markdown(f"**Title:** {row.Title}\n\n**Rating:** ⭐ {rating:.2f}\n\n**Genres:** {genres_with_commas}\n\n**Synopsis:** {truncated_synopsis}...")
+
+def truncate_text(text, max_chars):
+    return (text[:max_chars] + '...') if len(text) > max_chars else text
